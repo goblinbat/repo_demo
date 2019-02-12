c2880855fcdc5b770e2a305cfcc38c13ba5bfbdc
Molly Hopper <mollyhopper95@gmail.com>
2019-02-12 13:16:02 -0500

    madlib

---
+++ 17_madlib.py
@@ -0,0 +1,26 @@
+"""
+set variables to inputs asking for appropriate parts of speech
+
+what if i had a couple different prompts (madlibs) and used a random number generator to pick one each time?
+"""
+
+a = input('noun: ')
+b = input('adjective: ')
+c = input('past tense verb: ')
+d = input('verb: ')
+e = input('adjective: ')
+f = input('verb: ')
+g = input('adjective: ')
+h = input('quote: ')
+i = input('past tense verb: ')
+j = input('noun: ')
+
+prompt = f'Once upon a time, there was a little {a}. This {a} was very {b} and never {c}. One day, their mother \n' \
+    f'told them to {d}, but the {e} little {a} didnt want to {d}! Instead, they decided to {f}! When their {g} mother \n' \
+    f'learned of their disobedience, she told them "{h}" and {i} them without any {j}. From that day forth, \n' \
+    f'the {b} little {a} never disobeyed their mother to {f} again'
+
+prompt2 =  
+
+
+print(prompt)
